0fedcdbfdf571facb0fab2902bc6fc2488ca5870
soroko wilfried <wilfried14@outlook.com>
2022-04-10 22:36:14 +0300

    my project

---
+++ app/src/main/java/com/example/weahterforecast/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.weahterforecast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+/*
+#11103a  #2b5876  #4e4376
+*/
